1ff2adb205db5117cba1add13225f583a346e57f
pablo <you@example.com>
2020-10-23 10:29:46 -0300

    Updates

---
+++ precache-manifest.8437a1e224a48b3d3b4255cffb1d3e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dc1ce28110d54185c92950e66490ca",
+    "url": "/fronting/index.html"
+  },
+  {
+    "revision": "968f31c74d52a98c3f20",
+    "url": "/fronting/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc10f01dd9e953ad572c",
+    "url": "/fronting/static/js/2.1104b477.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/fronting/static/js/2.1104b477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968f31c74d52a98c3f20",
+    "url": "/fronting/static/js/main.cd16ff2e.chunk.js"
+  },
+  {
+    "revision": "d7ad56136db4a222dd0f",
+    "url": "/fronting/static/js/runtime-main.b66586f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fronting/static/media/logo.5d5d9eef.svg"
+  }
+]);
